8f50056b9af2e95bfe568cca555528dbea636275
karan <ka7982@rit.edu>
2023-10-16 15:46:19 -0400

    Working Lib.

--- tsmhc/app/Main.hs
+++ tsmhc/app/Main.hs
@@ -1,27 +1,18 @@
-import Lib
+{-# LANGUAGE DeriveAnyClass, PatternSynonyms, ViewPatterns, BangPatterns #-}
+-- import Lib
+import Lib (HashConsedClass, Hashconsed, hc, getValue, printTable, getTable)
 import Data.Hashable
-import Data.Bits (xor)  
+import Data.Bits (xor)
+import System.Mem
+import Control.Exception
 
 
-
-
-data BoolFormula = Var String | And BoolFormula BoolFormula | Or BoolFormula BoolFormula deriving (Eq, Show)
-instance Hashable BoolFormula where
-  hashWithSalt val (Var a) = hashWithSalt val a
-  hashWithSalt val (And a1 a2) = (hashWithSalt val a1 `xor` hashWithSalt val a2)`xor` hashWithSalt val a2
-  hashWithSalt val (Or a1 a2) = hashWithSalt val a1 `xor` hashWithSalt val a2
-
-
-data List a = Nil | Cons a (List a) deriving (Eq, Show)
-
-
-instance Hashable a => Hashable (List a) where
-  hashWithSalt _ Nil = 0
-  hashWithSalt val (Cons a l) = hashWithSalt val a `xor` hashWithSalt val l
-  hash Nil = 0
-  hash (Cons a l) = hash a `xor` hash l
-
-
+-- data List a = Nil | Cons a (List a) deriving (Eq, Show)
+-- instance Hashable a => Hashable (List a) where
+--   hashWithSalt _ Nil = 0
+--   hashWithSalt val (Cons a l) = hashWithSalt val a `xor` hashWithSalt val l
+--   hash Nil = 0
+--   hash (Cons a l) = hash a `xor` hash l
 
 -- testList :: IO ()
 -- testList = do
@@ -36,26 +27,100 @@ instance Hashable a => Hashable (List a) where
 --     print $ hc1 == hc2 -- true
 --     print $ hc1 == hc3 -- false
 
--- Sample test cases for BoolFormula
-boolFormulaTest :: IO ()
-boolFormulaTest = do
-    -- print "testing boolFormula: "
-    let table  = newEmptyTable
-    let formula1 = And (Var "p") (Var "q")
-    let formula2 = And (Var "p") (Var "q")
-    let formula3 = Or (Var "p") (Var "q")
-    let (hc1, table1) = lookupOrInsert formula1 table 
-    let (hc2, table2) = lookupOrInsert formula2 table1 
-    let (hc3, _) = lookupOrInsert formula3 table2 
-    print $ hc1 == hc2  -- Should print True, demonstrating memory saving
-    print $ hc1 == hc3  -- Should print False
+
+-- type Id = Hashconsed String
+
+type BoolFormula = Hashconsed BoolFormula'
+
+data BoolFormula' = 
+    Var' String 
+  | And' BoolFormula BoolFormula 
+  | Or' BoolFormula BoolFormula 
+  deriving (Eq, Show, HashConsedClass)
+
+instance Hashable BoolFormula' where
+  hashWithSalt val (Var' a) = hashWithSalt val a
+  hashWithSalt val (And' a1 a2) = (hashWithSalt val a1 `xor` hashWithSalt val a2)`xor` hashWithSalt (val + 10003344) a2
+  hashWithSalt val (Or' a1 a2) = hashWithSalt val a1 `xor` hashWithSalt val a2
+
+pattern Var :: String -> BoolFormula
+pattern Var str <- (getValue -> Var' str)
+  where
+    Var str = hc (Var' str)
+                                          
+pattern And :: BoolFormula -> BoolFormula -> BoolFormula
+pattern And x y <- (getValue -> (And' x y))
+  where
+    And x y = hc (And' x y)
+
+pattern Or :: BoolFormula -> BoolFormula -> BoolFormula
+pattern Or x y <- (getValue -> (Or' x y))
+  where
+    Or x y = hc (Or' x y)
+
+-- -- Sample test cases for BoolFormula
+-- boolFormulaTest :: IO ()
+-- boolFormulaTest = do
+--     print "testing boolFormula: "
+--     -- let table  = newEmptyTableW
+--     let varP = Var "p"
+--     let varPP = Var "p"
+--     let varQ = Var "q"
+--     let hc1 = And varP varQ
+--     let hc2 = And varP varQ
+--     let hc3 = Or varP varQ
+--     print hc1
+--     -- let hc2 = And varP varQ
+--     -- let hc3 = Or varP varQ
+--     -- -- let (hc1, table1) = lookupOrInsertW formula1 table 
+--     -- -- let (hc2, table2) = lookupOrInsertW formula2 table1 
+--     -- -- let (hc3, _) = lookupOrInsert formula3 table2 
+--     print $ hc1 == hc2  -- Should print True, demonstrating memory saving
+--     print $ hc1 == hc3  -- Should print False
+--     print $ varP == varPP  -- Should print False
 
 
 -- Main Function to run the tests
 main :: IO ()
 main = do
-        testList
-        boolFormulaTest
-  -- _ <- runTestTT listTest
-  -- boolFormulaTest
+  print "testing boolFormulaTest"
+  let !varP = Var "p"
+  let !varPP = Var "p"
+  let !varQ = Var "q"
+ 
+
+  -- printTable varQ
+  -- performMajorGC
+  let hc1 = And varP varQ
+  let hc2 = And varP varQ
+  let hc3 = Or varP varQ
+  print $ hc1 == hc2
+  printTable( getTable varQ)
+  print "Table printed"
+  -- printTable varP
+  performMajorGC
+  performGC
+  _ <- return ()
+  print "Starting Table print"
+  printTable(getTable varQ)
+  print "Table printed"
+  print $ varP == varPP
+  print "testing bool"
+
+  -- performMajorGC
+  -- putStrLn "Garbage collection performed"
+
+  -- putStrLn ""
+  -- putStrLn "Table:"
+  -- _ <- printTable table
+  -- putStrLn ""
+
+  -- _ <- clearTable table
+  -- putStrLn "Clearing Table"
+
+  -- print $ hc1 == hc2
+  -- print hc1
+  -- printTable table
+  -- putStrLn "Table End"
+
 
--- tsmhc/src/Lib.hs
+++ tsmhc/src/Lib.hs
@@ -1,24 +1,115 @@
-module Lib
-  ( Hashconsed(..)
-  , HashconsedTable
-  , newEmptyTable
-  , hashcons
-  ) where
+{-# LANGUAGE ScopedTypeVariables, AllowAmbiguousTypes, GADTs #-}
 
+module Lib(
+    Hashconsed,
+    HCTable,
+    newEmptyHCTable,
+    hashCons,
+    printTable,
+    finalizer,
+    HashConsedClass,
+    hc,
+    getValue, 
+    getTable
+    ) where
+
+import System.IO.Unsafe
 import Data.Hashable
 import qualified Data.HashMap.Strict as HashMap
+import System.Mem.Weak (Weak, mkWeakPtr, deRefWeak)
+import Control.Monad (forM_)
+import Control.Concurrent.MVar
 
--- | Data structure for hash consed values.
-data Hashconsed a = Hashconsed
-  { value :: a      -- ^ Original value
-  } deriving (Eq, Show)
+-- A newtype for the hash-consed value
+{-# ANN module ("HLint: ignore Use newtype instead of data" :: String) #-}
+data Hashconsed a where
+  Hashconsed :: (Eq a, Hashable a) => {
+    value :: !a
+  } -> Hashconsed a
+
+-- Define the hash logic of the hash-consed value
+instance Eq (Hashconsed a) where
+    (Hashconsed x) == (Hashconsed y) = x == y
+  
+-- Define the hash logic of the hash-consed value
+instance Hashable (Hashconsed a) where
+    hashWithSalt s (Hashconsed x) = hashWithSalt s x
+
+instance Show a => Show (Hashconsed a) where
+    show (Hashconsed x) = show x
+
+getValue :: Hashconsed a -> a
+getValue (Hashconsed val) = val
+                  
+-- Define the table type which holds weak references of hash-consed values
+type HCTable a = MVar (HashMap.HashMap Int (Weak (Hashconsed a)))
+
+newEmptyHCTable :: IO (HCTable a)
+newEmptyHCTable = newMVar HashMap.empty
+
+-- Function to insert or retrieve hash-consed values from the table
+hashCons :: Hashable a => a -> HCTable a -> IO (Hashconsed a)
+hashCons val tableRef = do
+  table <- readMVar tableRef
+  let hashVal = hash val -- Generating Hash of the value once
+  -- Lookup value from table
+  case HashMap.lookup hashVal table of
+    Just weakHC -> do
+      -- Get HashConsed Value from Weak Pointer
+      maybeHashconsed <- deRefWeak weakHC
+      case maybeHashconsed of
+        Just hConsed -> return hConsed
+        Nothing -> helper val hashVal tableRef 
+    Nothing -> helper val hashVal tableRef
+    where
+      helper valu hashVal hcTable = do
+        table <- takeMVar hcTable
+        weakRef <- mkWeakPtr (Hashconsed valu) (Just $ finalizer hashVal hcTable)
+        putMVar hcTable (HashMap.insert hashVal weakRef table)
+        tableRead <- readMVar hcTable
+        case HashMap.lookup hashVal tableRead of
+          Just weakHC -> do
+            maybeHashconsed <- deRefWeak weakHC
+            case maybeHashconsed of
+              Just hconsed -> return hconsed
+              Nothing -> return (Hashconsed valu)
+          Nothing -> return (Hashconsed valu)
+
+
+finalizer :: Int -> HCTable a -> IO ()
+finalizer key tableRef = do
+  putStrLn $ "removing value from table with key: " ++ show key
+  table <- takeMVar tableRef
+  putMVar tableRef (HashMap.delete key table)
+
+class (Eq a, Hashable a) => HashConsedClass a where
+  hashConsedTable :: HCTable a
+  hashConsedTable = unsafePerformIO newEmptyHCTable
+  {-# NOINLINE hashConsedTable #-}
+
+-- | Make a hash-consed value.
+hc ::  HashConsedClass a  => a -> Hashconsed a
+hc val = unsafePerformIO $ hashCons val hashConsedTable
+
+getTable :: HashConsedClass a => Hashconsed a -> HCTable a
+getTable _ = hashConsedTable
+
+printTable :: Show a => HCTable a -> IO ()
+printTable hcTable = do
+    table <- readMVar hcTable 
+    forM_ (HashMap.toList table) $ \(key, weakVal) -> do
+        maybeHC <- deRefWeak weakVal
+        case maybeHC of
+            Just hConsed -> putStrLn $ "Key: " ++ show key ++ " Value: " ++ show hConsed
+            Nothing -> putStrLn $ "Reference not found for Key: " ++ show key
+
 
 -- | Type alias for a hash consed table, mapping Int hash keys to `Hashconsed a` values.
-type HashconsedTable a = HashMap.HashMap a (Hashconsed a)
+type PureHashConsedTable a = HashMap.HashMap a (Hashconsed a)
 
 -- | Create a new, empty `HashconsedTable`.
-newEmptyTable :: HashconsedTable a
-newEmptyTable = HashMap.empty
+newEmptyPureHCTable :: PureHashConsedTable a
+newEmptyPureHCTable = HashMap.empty
 
 {-|
    Perform a lookup or insert operation on a `HashconsedTable`.
@@ -26,10 +117,10 @@ newEmptyTable = HashMap.empty
    If `val` is already in the table, return the existing `Hashconsed` value and the same table.
    If `val` is not in the table, insert it, return the new `Hashconsed` value and the modified table.
 -}
-hashcons :: Hashable a => a -> HashconsedTable a -> (Hashconsed a, HashconsedTable a)
-hashcons val table =
+hashconsPure :: Hashable a => a -> PureHashConsedTable a -> (Hashconsed a, PureHashConsedTable a)
+hashconsPure val table =
   case HashMap.lookup val table of
-    Just hc -> (hc, table)  -- Value found, return existing Hashconsed and original table
+    Just hConsed -> (hConsed, table)  -- Value found, return existing Hashconsed and original table
     Nothing -> 
-      let hc = Hashconsed val
-      in (newHc, HashMap.insert val hc table)  -- Value not found, insert and return new table
+      let newHC = Hashconsed val
+      in (newHC, HashMap.insert val newHC table)  -- Value not found, insert and return new table
